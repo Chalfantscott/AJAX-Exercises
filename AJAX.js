c25dcd1bd49cd288c148de1b43ab9512a98886bb
Scott Chalfant <chalfantscott@scotts-mbp-2.home>
2018-04-23 21:14:21 -0400

    added dropdown menu

--- AJAX.js
+++ AJAX.js
@@ -1,7 +1,15 @@
 $(function() {
-    $.get('https://dog.ceo/api/breeds/list', function(data){
-        data.message.forEach(function(currentBreed){
-            $('select').append('<option val="' + currentBreed + '">"' + currentBreed + '"</option>');
+    $.get('https://dog.ceo/api/breeds/list', function(response){
+        response.message.forEach(function(currentBreed){
+            $('select').append('<option val="'+ currentBreed +'">"'+ currentBreed +'"</option>');
+        });
+    });
+
+    $('select').change(function(){
+        var dogBreed = $(this).val();
+        var url = "https://dog.ceo/api/breed/"+ dogBreed +"/images/random";
+        $.get(url, function(response){
+            $('body').append('<img src="'+ response.message +'"/>')
         });
     });
 
